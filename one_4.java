4fa161b40d8417b70cae9e567e54b3ad493af383
max_moskalenko <mmoskalenko21@gmail.com>
2023-07-13 11:29:20 -0400

    trying 1.4

--- one_4.java
+++ one_4.java
@@ -1,4 +1,4 @@
-/*
+/*x
 Palindrome Permutation: Given a string, write a function to check if 
 it is a permutation of a palin­drome. A palindrome is a word or 
 phrase that is the same forwards and backwards. A permutation 
@@ -12,13 +12,59 @@ Output: True (permutations: "taco cat", "atco eta", etc.)
 public class one_4 {
     public static void main(String args[]) { //type psvm to shortcut
         String input = "taco cat";
-        System.out.println(isPalindrome(input));
-        System.out.println(input);
-        permutations("", input);
+        char str[] = input.toCharArray();
+        //System.out.println(isPalindrome(input));
+        //System.out.println(input);
+        int n = str.length;
+        findPermutations(str, 0, n);
+        //permutations("", input);
         
     }
 
 
+// Returns true if str[curr] does not matches with any of the
+// characters after str[start]
+static boolean shouldSwap(char str[], int start, int curr) {
+    for (int i = start; i < curr; i++) {
+        if (str[i] == str[curr]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+// Prints all distinct permutations in str[0..n-1]
+static void findPermutations(char str[], int index, int n) {
+    if (index >= n) {
+        if (isPalindrome(str)) {
+        System.out.println(str);
+        return;
+    }
+    return;
+        
+    }
+
+    for (int i = index; i < n; i++) {
+
+        // Proceed further for str[i] only if it
+        // doesn't match with any of the characters
+        // after str[index]
+        boolean check = shouldSwap(str, index, i);
+        if (check) {
+            swap(str, index, i);
+            findPermutations(str, index + 1, n);
+            swap(str, index, i);
+        }
+    }
+}
+
+static void swap(char[] str, int i, int j) {
+    char c = str[i];
+    str[i] = str[j];
+    str[j] = c;
+}
+
+/* 
     static void permutations(String processed, String un_processed) {
         if (un_processed.isEmpty() && isPalindrome(processed)) {
     
@@ -35,15 +81,19 @@ public class one_4 {
         }
         
     }
-
-    static boolean isPalindrome(String str) {
+*/
+    static boolean isPalindrome(char[] arr) {
+        String str = "";
+        for (char s : arr) {
+            str += s;
+        }
         str = str.replaceAll("\\s","");
-        System.out.println(str);
+    
         String new_str = "";
         for(int i = str.length() -1; i >= 0; i--) {
             new_str += str.charAt(i); 
         }
-        System.out.println(new_str);
+
         return (str.compareTo(new_str) == 0);
     }
 
---
+++ one_4real.java
@@ -0,0 +1,79 @@
+/*x
+Palindrome Permutation: Given a string, write a function to check if 
+it is a permutation of a palin­drome. A palindrome is a word or 
+phrase that is the same forwards and backwards. A permutation 
+is a rearrangement of letters. The palindrome does not need to be
+limited to just dictionary words.
+EXAMPLE
+Input: Tact Coa
+Output: True (permutations: "taco cat", "atco eta", etc.)
+*/
+
+public class one_4real {
+    public static void main(String args[]) { //type psvm to shortcut
+        String input = "taco cat";
+        char str[] = input.toCharArray();
+        int n = str.length;
+        findPermutations(0, str, 0, n);    
+    }
+    // Returns true if str[curr] does not matches with any of the
+    // characters after str[start]
+    static boolean shouldSwap(char str[], int start, int curr) {
+        for (int i = start; i < curr; i++) {
+            if (str[i] == str[curr]) {
+                return false;
+            }
+        }
+        return true;
+    }
+    // Prints all distinct permutations in str[0..n-1]
+    static void findPermutations(int checker, char str[], int index, int n) {
+        if (index >= n) {
+            if (isPalindrome(str)) {
+                if (checker == 0) {
+                    System.out.print("True (permutations: ");
+                    checker++;
+                }
+                System.out.print("\"");
+                System.out.print(str);
+                System.out.println("\"");
+
+            return;
+        }
+        return;  
+        }
+        for (int i = index; i < n; i++) {
+            // Proceed further for str[i] only if it
+            // doesn't match with any of the characters
+            // after str[index]
+            boolean check = shouldSwap(str, index, i);
+            if (check) {
+                swap(str, index, i);
+                findPermutations(checker,str, index + 1, n);
+                swap(str, index, i);
+            }
+        }
+    }
+
+    static void swap(char[] str, int i, int j) {
+        char c = str[i];
+        str[i] = str[j];
+        str[j] = c;
+    }
+
+    static boolean isPalindrome(char[] arr) {
+        String str = "";
+        for (char s : arr) {
+            str += s;
+        }
+        str = str.replaceAll("\\s","");
+        
+        String new_str = "";
+        for(int i = str.length() -1; i >= 0; i--) {
+            new_str += str.charAt(i); 
+        }
+
+        return (str.compareTo(new_str) == 0);
+    }
+}
+
